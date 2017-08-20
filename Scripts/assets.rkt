34f29a6945dd7387f5cc67709fa785f546b4bdfb
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-08-20 15:33:50 +0800

    rename lambda400.png SplashScreen-400x400.png

--- Scripts/assets.rkt
+++ Scripts/assets.rkt
@@ -6,4 +6,4 @@
 (define-runtime-path assets "../StartsWithPanel/Assets")
 
 (define λicon (lambda-icon #:height 400))
-(send λicon save-file (build-path assets "lambda400.png")'png)
+(send λicon save-file (build-path assets "SplashScreen-400x400.png")'png)
