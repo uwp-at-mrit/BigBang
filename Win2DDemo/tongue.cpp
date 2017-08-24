5dc79a6e67170e1b84b1719c181eb6674cc2e4fc
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-08-24 09:18:10 +0800

    remove tongue.cpp

--- Win2DDemo/tongue.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "pch.h"
-#include "tongue.h"
-
-static ResourceLoader^ tongue = nullptr;
-
-String^ Win2D::UWRT::Tongue::Speak(String^ id) {
-    if (tongue == nullptr) tongue = ResourceLoader::GetForCurrentView();
-    return tongue->GetString(id);
-}
