0ada80396a53f6805e4bc4540c1cc864190ac197
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-09-12 19:41:25 +0800

    typos

--- Scripts/assets.rkt
+++ Scripts/assets.rkt
@@ -3,7 +3,7 @@
 (require images/icons/symbol)
 (require racket/runtime-path)
 
-(define-runtime-path assets "../StartsWithPanel/Assets")
+(define-runtime-path assets "../WinACS/Assets")
 
 (define λicon (lambda-icon #:height 400))
 (send λicon save-file (build-path assets "SplashScreen-400x400.png")'png)
