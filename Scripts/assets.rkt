c2e19fc4a577519d4fddba7e056fe14b5f033f5e
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-09-14 06:11:02 +0800

    delete Script/assets.rkt

--- Scripts/assets.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(require images/icons/symbol)
-(require racket/runtime-path)
-
-(define-runtime-path assets "../WinACS/Assets")
-
-(define λicon (lambda-icon #:height 400))
-(send λicon save-file (build-path assets "SplashScreen-400x400.png")'png)
