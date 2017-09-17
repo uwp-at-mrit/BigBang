b13ad722f00902028c80c8470966c884d0575be2
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-09-17 23:15:05 +0800

    improve tcp client and server

--- Prototype/plc.rkt
+++ Prototype/plc.rkt
@@ -1,12 +1,27 @@
 #lang racket
 
 (require racket/tcp)
+(require racket/date)
 
-(define-values (/dev/tcpin /dev/tcpout) (tcp-connect/enable-break "172.16.8.195" 18030))
+(date-display-format 'iso-8601)
 
-(write "Hello, Visual C++/CX, I am Racket!" /dev/tcpout)
-(newline /dev/tcpout)
-(flush-output /dev/tcpout)
-(tcp-abandon-port /dev/tcpout)
-(read-line /dev/tcpin)
-(tcp-abandon-port /dev/tcpin)
+(with-handlers ([exn:break? void])
+  (let connect-send-wait-loop ()
+    (with-handlers ([exn:fail? (λ [e] (fprintf (current-error-port) "~a~n" (exn-message e)))])
+      (parameterize ([current-custodian (make-custodian)])
+        (dynamic-wind
+         (thunk (void))
+         (thunk (let-values ([(/dev/tcpin /dev/tcpout) (tcp-connect/enable-break "172.16.8.1" 18030)])
+                  (file-stream-buffer-mode /dev/tcpout 'line)
+                  
+                  (let ([now (seconds->date (* (current-inexact-milliseconds) 0.001))])
+                    (define ms (~a (exact-round (/ (date*-nanosecond now) 1000000)) #:min-width 3 #:left-pad-string "0"))
+                    (fprintf /dev/tcpout "[~a.~a]Hello, I am Racket!~n" (date->string now #true) ms)
+                    
+                    (if (sync/timeout/enable-break 1.618 /dev/tcpin)
+                        (displayln (read-line /dev/tcpin))
+                        (error 'plc "[~a.~a]Handshaking Timeout!" (date->string now #true) ms)))))
+         (thunk (custodian-shutdown-all (current-custodian))))))
+    
+    (sleep 1)
+    (connect-send-wait-loop)))
--- Scripts/tcpserver.rkt
+++ Scripts/tcpserver.rkt
@@ -3,16 +3,19 @@
 (require racket/tcp)
 
 (define (server)
-  (define listener (tcp-listen 18030 4 #true))
-  (define-values (/dev/tcpin /dev/tcpout) (tcp-accept/enable-break listener))
-
-  (define-values (local lport remote rport) (tcp-addresses /dev/tcpout #true))
-  (displayln (read-line /dev/tcpin))
-  (fprintf /dev/tcpout "Greetings, ~a:~a~n" remote rport)
-  (flush-output /dev/tcpout)
-  (tcp-abandon-port /dev/tcpin)
-  (tcp-abandon-port /dev/tcpout)
-  (tcp-close listener)
+  (with-handlers ([exn:fail? (λ [e] (fprintf (current-error-port) "~a~n" (exn-message e)))])
+    (parameterize ([current-custodian (make-custodian)])
+      (define listener (tcp-listen 18030 256 #true))
+      (dynamic-wind
+       (thunk (void))
+       (thunk (let-values ([(/dev/tcpin /dev/tcpout) (tcp-accept/enable-break listener)])
+                (file-stream-buffer-mode /dev/tcpout 'line)
+                
+                (define-values (local lport remote rport) (tcp-addresses /dev/tcpout #true))
+                (displayln (read-line /dev/tcpin))
+                (fprintf /dev/tcpout "Greetings, ~a:~a, I am Racket Too.~n" remote rport)))
+       (thunk (custodian-shutdown-all (current-custodian))))))
   (server))
 
-(server)
+(with-handlers ([exn? void])
+  (server))
