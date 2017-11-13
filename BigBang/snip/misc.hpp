9fc7beee4bde4a17b826c404ec489af10daf2c7c
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-11-13 21:45:55 +0800

    remove HFlippedPipeSnip

--- BigBang/snip/misc.hpp
+++ BigBang/snip/misc.hpp
@@ -8,11 +8,3 @@
 Windows::Foundation::Rect snip_enclosing_box(
     WarGrey::SCADA::Snip* snip, float x, float y,
     Windows::Foundation::Numerics::float3x2 tf);
-
-Windows::Foundation::Rect snip_socket_hflip(
-    WarGrey::SCADA::Snip* snip, float x, float y,
-    Windows::Foundation::Rect& socket);
-
-void snip_draw_with_hflipping(WarGrey::SCADA::Snip* snip,
-    Microsoft::Graphics::Canvas::CanvasDrawingSession^ ds,
-    float x, float y, float Width, float Height);
--- BigBang/snip/pipeline/pipesnip.hpp
+++ BigBang/snip/pipeline/pipesnip.hpp
@@ -11,43 +11,6 @@ namespace WarGrey::SCADA {
         virtual Windows::Foundation::Rect get_output_port() = 0;
     };
 
-    template <class PS> // TODO: workout a better design pattern
-    private class HFlippedPipeSnip : public WarGrey::SCADA::IPipeSnip {
-    public:
-        HFlippedPipeSnip(PS* self) : master(self) {}
-        ~HFlippedPipeSnip() noexcept { delete this->master; }
-        PS* self() { return this->master; }
-
-    public:
-        void load() override {
-            this->master->load();
-        }
-
-        void update(long long count, long long interval, long long uptime, bool is_slow) override {
-            this->master->update(count, interval, uptime, is_slow);
-        }
-
-        void fill_extent(float x, float y, float* w = nullptr, float* h = nullptr) override {
-            this->master->fill_extent(x, y, w, h);
-        }
-
-        void draw(Microsoft::Graphics::Canvas::CanvasDrawingSession^ ds, float x, float y, float Width, float Height) override {
-            snip_draw_with_hflipping(this->master, ds, x, y, Width, Height);
-        }
-
-    public:
-        Windows::Foundation::Rect get_input_port() override {
-            return snip_socket_hflip(this->master, 0.0F, 0.0F, this->master->get_input_port());
-        }
-
-        Windows::Foundation::Rect get_output_port() override {
-            return snip_socket_hflip(this->master, 0.0F, 0.0F, this->master->get_output_port());
-        }
-    
-    private:
-        PS* master;
-    };
-
     void pipe_connecting_position(
         WarGrey::SCADA::IPipeSnip* prev,
         WarGrey::SCADA::IPipeSnip* pipe,
