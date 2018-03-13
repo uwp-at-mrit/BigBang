3d07c8702cf8b833f3cd54f418be79dd96ea43fb
WarGrey Ju <juzhenliang@gmail.com>
2018-03-13 15:49:24 +0800

    clear 63FT

--- 63FT.Boat/command.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "menu.idl"
-
-namespace WarGrey::SCADA {
-	private enum class Menu { Start, Stop };
-
-	WarGrey::SCADA::CommandMenu<Menu>* make_start_stop_menu(WarGrey::SCADA::IMenuCommand<Menu>* exe);
-}
