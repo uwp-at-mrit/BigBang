95a18aaeed92d2df891c142d3aa75bde6840eb5e
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-03-16 11:21:58 +0800

    manually change control points number

--- Scripts/Prototype/yacht.rkt
+++ Scripts/Prototype/yacht.rkt
@@ -22,116 +22,100 @@
               (pt-transform (make-intermediate-point t)))
             (map pt-transform (cons point others)))))
 
+(define draw-curve
+  (lambda [dc src curve-nodes point-nodes diameter]
+    (send dc set-pen (make-pen #:color "Gainsboro"))
+    (send dc draw-lines src)
+    (for ([control-point (in-list (take (cdr point-nodes) (- (length point-nodes) 2)))])
+      (draw-endpoint dc control-point diameter))
+    (send dc set-pen (make-pen #:color "Green"))
+    (draw-endpoint dc (car point-nodes) diameter)
+    (send dc set-pen (make-pen #:color "Red"))
+    (draw-endpoint dc (last point-nodes) diameter)
+    (send dc set-pen (make-pen #:color "Black"))
+    (send dc draw-lines curve-nodes)))
+
 (define parts
   (for/list ([vertices.txt (in-list (list "deck_bottom.rktl" "deck_top.rktl" "cabin_rt.rktl"))])
     (call-with-input-file* vertices.txt read)))
 
-(define bezier%
+(define yacht%
   (class canvas% (super-new)
-    (init-field parts [diameter 4.0] [samples #false])
-    
+    (init-field parts [diameter 4.0] [max-count 64] [samples #false])
+
     (define-values (width height) (values 1 1))
     (define sample-count (or samples 128))
-    (define image (send this make-bitmap width height))
-    (define dc (send image make-dc))
     (define pt-transform (void))
-    (define saved-points (make-hash))
+    (define images (make-hash))
 
+    (define point-count 2)
+    (define curve-count (length parts))
+    (define counts (append (range 2 (add1 max-count)) (range max-count 2 -1)))
+    (define count-size (length counts))
+    (define count-idx 0)
+
+    (define timer-running? #false)
     (define ghostcat
       (make-object timer%
-        (let* ([max-count 64]
-               [counts (append (range 2 max-count) (range (sub1 max-count) 2 -1))]
-               [size (length counts)]
-               [idx 0])
-          (thunk (let ([count (list-ref counts (remainder idx size))])
-                   (send (send this get-parent) set-label (format "Yacht(~a, ~a, ~a)" width height count))
-                   (draw-bezier dc count)
-                   (set! idx (add1 idx)))))))
-    
+        (thunk (let ([window (send this get-parent)])
+                 (set! count-idx (add1 count-idx))
+                 (set! point-count (list-ref counts (remainder count-idx count-size)))
+                 (send this refresh)))))
+  
     (define/override (on-paint)
-      (send (send this get-dc) draw-bitmap image 0 0))
+      (when (procedure? pt-transform)
+        (define dc (send this get-dc))
+        (for ([seq (in-range curve-count)])
+          (define key (list seq point-count width height))
+          (define image
+            (hash-ref! images key
+                       (thunk (thread (thunk (let ([src (list-ref parts seq)])
+                                               (define points (vertices-take src point-count))
+                                               (define image (send this make-bitmap width height))
+                                               (define dc (send image make-dc))
+                                               (send dc set-smoothing 'smoothed)
+                                               (define-values (curve-nodes point-nodes)
+                                                 (compute-curve-points points sample-count pt-transform))
+                                               (draw-curve dc (map pt-transform src) curve-nodes point-nodes diameter)
+                                               (hash-set! images key image)
+                                               (send this refresh)))))))
+        
+          (cond [(thread? image) (send dc draw-text (format "Drawing curves with ~a control points" point-count) 0 0 #true)]
+                [else (send* dc (draw-bitmap image 0 0) (draw-text (format "Control points: ~a" point-count) 0 0 #true))]))))
 
+    (define/override (on-char keyboard)
+      (case (send keyboard get-key-release-code)
+        [(left) (manually-animate-curves sub1)]
+        [(right) (manually-animate-curves add1)]
+        [(#\space) (cond [(and timer-running?) (send ghostcat stop)]
+                         [else (send ghostcat start (quotient 1000 16))])
+                   (set! timer-running? (not timer-running?))]))
+    
     (define/override (on-size w h)
-      (hash-clear! saved-points)
+      (clear-images)
       (set!-values (width height) (values w h))
       (set! pt-transform (make-point-transform x0 y0 xn yn width height diameter))
       (set! sample-count (or samples (max width height)))
-
-      (set! image (send this make-bitmap width height))
-      (set! dc (send image make-dc))
-      (send dc set-smoothing 'smoothed))
+      (send (send this get-parent) set-label (format "Yacht(~a, ~a)" width height)))
 
     (define/override (on-superwindow-show shown?)
-      (cond [(not shown?) (send ghostcat stop)]
-            [else (send ghostcat start (quotient 1000 16))]))
+      (unless (and shown?)
+        (send ghostcat stop)
+        (clear-images)))
 
-    (define/private (draw-bezier dc count)
-      (send dc clear)
-      (for ([points (in-list parts)]
-            [seq (in-naturals)])
-        (define curve.point-nodes
-          (hash-ref! saved-points (cons seq count)
-                     (thunk (let-values ([(curve-nodes point-nodes) (compute-curve-points points sample-count pt-transform)])
-                              (cons curve-nodes point-nodes)))))
-        (define-values (curve-nodes point-nodes) (values (car curve.point-nodes) (cdr curve.point-nodes)))
+    (define/private (clear-images)
+      (for ([image (in-hash-values images)])
+        (when (thread? image)
+          (kill-thread image)))
+      (hash-clear! images))
 
-        (send dc set-pen (make-pen #:color "Gainsboro"))
-        (for ([control-point (in-list (take (cdr point-nodes) (- (length points) 2)))])
-          (draw-endpoint dc control-point diameter))
-        (send dc set-pen (make-pen #:color "Green"))
-        (draw-endpoint dc (car point-nodes) diameter)
-        (send dc set-pen (make-pen #:color "Red"))
-        (draw-endpoint dc (last point-nodes) diameter)
-        (send dc set-pen (make-pen #:color "Black"))
-        (send dc draw-lines curve-nodes)
-        (send this refresh)))))
-
-(define yacht%
-  (class canvas% (super-new)
-    (init-field parts [diameter 4.0] [samples #false])
-    
-    (define-values (width height) (values 1 1))
-    (define image (send this make-bitmap width height))
-    (define pt-transform (void))
-
-    (define ghostcat (thread void))
-    
-    (define/override (on-paint)
-      (send (send this get-dc) draw-bitmap image 0 0))
-
-    (define/override (on-size w h)
-      (set!-values (width height) (values w h))
-      (set! pt-transform (make-point-transform x0 y0 xn yn width height diameter))
-      (send (send this get-parent) set-label (format "Yacht(~a, ~a)" width height))
-
-      (set! image (send this make-bitmap width height))
-      (define dc (send image make-dc))
-      (send dc set-smoothing 'smoothed)
-
-      (when (thread-running? ghostcat) (kill-thread ghostcat))
-      (set! ghostcat (thread (thunk (draw-bezier dc)))))
-
-    (define/override (on-superwindow-show shown?)
-      (when (not shown?)
-        (when (thread-running? ghostcat)
-          (kill-thread ghostcat))))
-
-    (define/private (draw-bezier dc)
-      (for ([points (in-list parts)])
-        (define-values (curve-nodes point-nodes)
-          (compute-curve-points points (or samples (max width height)) pt-transform))
-        (send dc set-pen (make-pen #:color "Gainsboro"))
-        (for ([control-point (in-list (take (cdr point-nodes) (- (length points) 2)))])
-          (draw-endpoint dc control-point diameter))
-        (send dc set-pen (make-pen #:color "Green"))
-        (draw-endpoint dc (car point-nodes) diameter)
-        (send dc set-pen (make-pen #:color "Red"))
-        (draw-endpoint dc (last point-nodes) diameter)
-        (send dc set-pen (make-pen #:color "Black"))
-        (send dc draw-lines curve-nodes)
-        (send this refresh)))))
+    (define/private (manually-animate-curves op)
+      (set! count-idx (op count-idx))
+      (when (< count-idx 0) (set! count-idx 0) (bell))
+      (set! point-count (list-ref counts (remainder count-idx count-size)))
+      (send this refresh))))
 
 (define universe (new frame% [label "Yacht"] [min-width 512] [min-height 512]))
-(define yacht (new bezier% [parent universe] [parts parts] [samples 64]))
+(define yacht (new yacht% [parent universe] [parts parts] [samples 64]))
 
 (send* universe (center 'both) (show #true) (maximize #true))
