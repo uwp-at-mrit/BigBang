ce9f61d0dc67bff3fc81b8c893cf56903acdacf1
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-04-19 09:18:43 +0800

    mrit/tcpserver.rkt is ready

--- Scripts/mrit/tcpserver.rkt
+++ Scripts/mrit/tcpserver.rkt
@@ -2,6 +2,11 @@
 
 (require racket/tcp)
 
+(struct mrmsg (code block addr0 addrn data) #:transparent)
+
+(define memory (make-bytes 0))
+(define header (make-bytes (+ 2 2 2 2 2)))
+(define tail (make-bytes 4))
 
 (define read-mrmsg
   (lambda [/dev/tcpin]
@@ -14,7 +19,30 @@
     (define data (read-bytes size /dev/tcpin))
     (define checksum (integer-bytes->integer (read-bytes 2 /dev/tcpin) #false #true))
     (define EOM (integer-bytes->integer (read-bytes 2 /dev/tcpin) #false #true))
-    (list head code DB addr0 addrn data checksum EOM)))
+    (mrmsg code DB addr0 addrn data)))
+
+(define write-mrmsg
+  (lambda [/dev/tcpout code db addr0 addrn]
+    (define dsize (- addrn addr0 -1))
+    
+    (bytes-set! header 0 (char->integer #\$))
+    (bytes-set! header 1 code)
+    (integer->integer-bytes db 2 #false #true header 2)
+    (integer->integer-bytes addr0 2 #false #true header 4)
+    (integer->integer-bytes addrn 2 #false #true header 6)
+    (integer->integer-bytes dsize 2 #false #true header 8)
+    
+    (integer->integer-bytes #xFFFF 2 #false #true tail 0)
+    (bytes-set! tail 2 (char->integer #\return))
+    (bytes-set! tail 3 (char->integer #\newline))
+
+    (write-bytes header /dev/tcpout)
+    (write-bytes memory /dev/tcpout addr0 (+ addrn 1))
+    (write-bytes tail /dev/tcpout)
+
+    (flush-output /dev/tcpout)
+
+    (+ (bytes-length header) dsize (bytes-length tail))))
 
 (define (server)
   (with-handlers ([exn:fail? (λ [e] (fprintf (current-error-port) "~a~n" (exn-message e)))])
@@ -25,11 +53,17 @@
                 (define-values (hostname port _r _p) (tcp-addresses listener #true))
                 (printf "> ~a:~a~n" hostname port)
                 (define-values (/dev/tcpin /dev/tcpout) (tcp-accept/enable-break listener))
-                (file-stream-buffer-mode /dev/tcpout 'line)
                 
                 (define-values (local lport remote rport) (tcp-addresses /dev/tcpout #true))
                 (define message (read-mrmsg /dev/tcpin))
-                (writeln message)))
+                (define-values (addr0 addrn) (values (mrmsg-addr0 message) (mrmsg-addrn message)))
+
+                (when (> addrn (bytes-length memory)) (set! memory (make-bytes (+ addrn 1))))
+                (for ([i (in-range addr0 (+ addrn 1))]) (bytes-set! memory i (random 0 256)))
+                
+                (printf ">> [sent ~a bytes to ~a:~a]~n"
+                        (write-mrmsg /dev/tcpout (mrmsg-code message) (mrmsg-block message) addr0 addrn)
+                        remote rport)))
        (thunk (custodian-shutdown-all (current-custodian))))))
   (sleep 1)
   (server))
