0460a9da11d67a6a4076e8cd910ff53c736836c9
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-04-21 12:02:24 +0800

    enhanced the MRIT Protocol simulator

--- Scripts/mrit/slave.rkt
+++ Scripts/mrit/slave.rkt
@@ -3,7 +3,9 @@
 (require "message.rkt")
 
 (require racket/tcp)
-(require file/sha1)
+
+(define memory (make-bytes 0))
+(define master-ipv4 (vector-ref (current-command-line-arguments) 0))
 
 (with-handlers ([exn:break? void])
   (let connect-send-wait-loop ()
@@ -11,12 +13,21 @@
       (parameterize ([current-custodian (make-custodian)])
         (dynamic-wind
          (thunk (void))
-         (thunk (let-values ([(/dev/tcpin /dev/tcpout) (tcp-connect/enable-break "192.168.8.132" 2100)])
+         (thunk (let-values ([(/dev/tcpin /dev/tcpout) (tcp-connect/enable-break master-ipv4 2000)])
                   (define-values (local lport remote rport) (tcp-addresses /dev/tcpout #true))
                   (printf "[connected to ~a:~a]~n" remote rport)
 
-                  (define-values (signature data) (read-mrmsg /dev/tcpin))
-                  (displayln signature)))
+                  (let wait-read-response-loop ()
+                    (define-values (signature _) (read-mrmsg /dev/tcpin 40))
+                    (define-values (addr0 addrn) (values (mrmsg-addr0 signature) (mrmsg-addrn signature)))
+                    
+                    (when (> addrn (bytes-length memory)) (set! memory (make-bytes (+ addrn 1))))
+                    (for ([i (in-range addr0 (+ addrn 1))]) (bytes-set! memory i (random 0 256)))
+                    
+                    (printf ">> [sent ~a bytes to ~a:~a]~n"
+                            (write-mrmsg /dev/tcpout (mrmsg-code signature) (mrmsg-block signature) addr0 addrn memory)
+                            remote rport)
+                    (wait-read-response-loop))))
          (thunk (custodian-shutdown-all (current-custodian))))))
     
     (sleep 1)
