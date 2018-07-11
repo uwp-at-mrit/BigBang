a9a9cef5c828abaeaa5a95ea8b866e68b03c99c5
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-07-11 20:31:55 +0800

    deal with CSV

--- Scripts/data/excel.rkt
+++ Scripts/data/excel.rkt
@@ -1,21 +1,26 @@
 #lang racket/gui
 
+(provide read-xlsx)
+
 (require racket/pretty)
 (require simple-xlsx)
 
-(define src.xlsx (get-file))
-
-(when (path? src.xlsx)
-  (define dest.rktl (path-replace-extension src.xlsx ".rktl"))
-
+(define (read-xlsx src.xlsx)
   (define metrics (make-hasheq))
   
-  (with-input-from-xlsx-file src.xlsx
-    (lambda [data.xlsx]
-      (for ([sheet (in-list (get-sheet-names data.xlsx))])
-        (printf "exporting ~a...~n" sheet)
-        (load-sheet sheet data.xlsx)
-        (hash-set! metrics (string->symbol sheet) (get-sheet-rows data.xlsx)))))
+  (when (path? src.xlsx)
+    (with-input-from-xlsx-file src.xlsx
+      (lambda [data.xlsx]
+        (for ([sheet (in-list (get-sheet-names data.xlsx))])
+          (printf "exporting ~a...~n" sheet)
+          (load-sheet sheet data.xlsx)
+          (hash-set! metrics (string->symbol sheet) (get-sheet-rows data.xlsx))))))
+
+  metrics)
+
+(module+ main
+  (define src.xlsx (get-file))
+  (define dest.rktl (path-replace-extension src.xlsx ".rktl"))
 
   (with-output-to-file dest.rktl #:exists 'truncate/replace
-    (thunk (pretty-write metrics))))
+    (thunk (pretty-write (read-xlsx src.xlsx)))))
