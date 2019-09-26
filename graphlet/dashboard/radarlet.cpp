98ce10dfc27d568462a00db8ffbd9b7f18ed628d
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-09-26 11:06:57 +0800

    finish Radarlet

--- graphlet/dashboard/radarlet.cpp
+++ graphlet/dashboard/radarlet.cpp
@@ -19,40 +19,145 @@ using namespace Microsoft::Graphics::Canvas::Text;
 using namespace Microsoft::Graphics::Canvas::Brushes;
 using namespace Microsoft::Graphics::Canvas::Geometry;
 
-static const long long DEFAULT_SLOT_SIZE = 4096LL;
-static const unsigned int DEFAULT_COUNT_RATE = 5;
+static const double radian0 = -pi / 2.0;
+static const float caption_size = 16.0F;
 
-static CanvasSolidColorBrush^ lines_default_border_color = Colours::make(0xBBBBBB);
-static CanvasTextFormat^ lines_default_font = make_bold_text_format(12.0F);
-static CanvasTextFormat^ lines_default_legend_font = make_bold_text_format(14.0F);
+static RadarStyle default_radar_style;
+
+static CanvasSolidColorBrush^ radar_default_color = Colours::DodgerBlue;
+static CanvasSolidColorBrush^ radar_default_label_color = Colours::Snow;
+static CanvasSolidColorBrush^ radar_default_ring_color = Colours::GrayText;
 
 /*************************************************************************************************/
-IRadarlet::IRadarlet(float radius, unsigned int count) : radius(radius), count(count) {}
+IRadarlet::IRadarlet(float radius, unsigned int count, float* initials)
+	: IRadarlet(radius, count, default_radar_style, initials) {}
+
+IRadarlet::IRadarlet(float radius, unsigned int count, RadarStyle& style, float* initials) : Radius(radius), count(count), style(style) {
+	this->scales = new float[this->count];
+	this->captions = ref new Platform::Array<Platform::String^>(this->count);
+
+	if (initials != nullptr) {
+		for (unsigned int idx = 0; idx < this->count; idx++) {
+			this->scales[idx] = flmax(flmin(initials[idx], 1.0F), 0.0F);
+		}
+	} else {
+		memset(this->scales, 0, this->count * sizeof(float));
+	}
+}
+
+IRadarlet::~IRadarlet() {
+	delete[] this->scales;
+}
 
 void IRadarlet::construct() {
+	Platform::Array<CanvasGeometry^>^ labels = ref new Platform::Array<CanvasGeometry^>(this->count);
+	CanvasGeometry^ caption = blank();
+	double rad_step = 2.0 * pi / double(this->count);
+	float text_height = 0.0F;
+	TextExtent te;
 
+	this->prepare_style();
+	for (unsigned int idx = 0; idx < this->count; idx++) {
+		CanvasGeometry^ origin = paragraph(this->captions[idx], this->style.font, &te);
+
+		text_height = flmax(text_height, te.height);
+		labels[idx] = geometry_translate(origin, te.width * -0.5F, te.height * -0.5F);
+	}
+
+	this->radius = this->Radius - text_height * 0.5F;
+	for (unsigned int idx = 0; idx < this->count; idx++) {
+		float radian = float(radian0 + rad_step * double(idx));
+		double angle = 360.0 * double(idx) / double(this->count);
+		float ix, iy;
+
+		circle_point(this->radius, radian, &ix, &iy);
+		caption = geometry_union(caption, geometry_rotate(labels[idx], angle, 0.0F, 0.0F), ix, iy);
+	}
+
+	this->caption = geometry_freeze(caption);
+	this->radius = this->Radius - text_height;
+	this->reshape();
 }
 
 void IRadarlet::fill_extent(float x, float y, float* w, float* h) {
-	SET_BOXES(w, h, this->radius * 2.0F);
+	SET_BOXES(w, h, this->Radius * 2.0F);
 }
 
 void IRadarlet::draw(CanvasDrawingSession^ ds, float x, float y, float Width, float Height) {
-	float cx = x + this->radius;
-	float cy = y + this->radius;
-	double radian0 = -pi / 2.0;
 	double rad_step = 2.0 * pi / double(this->count);
-	float r = this->radius - 1.0F;
+	float cx = x + this->Radius;
+	float cy = y + this->Radius;
+	
+	ds->DrawCachedGeometry(this->caption, cx, cy, this->style.label_color);
 
-	for (unsigned int c = 1; c < this->count; c++) {
-		ds->DrawCircle(cx, cy, r * float(c) / float(this->count), Colours::Azure, 1.0F);
+	for (unsigned int c = 1; c <= this->count; c++) {
+		ds->DrawCircle(cx, cy, this->radius * float(c) / float(this->count), this->style.ring_color, 1.0F);
 	}
 
 	for (unsigned int idx = 0; idx < this->count; idx++) {
 		float radian = float(radian0 + rad_step * double(idx));
 		float ix, iy;
 
-		circle_point(r, radian, &ix, &iy);
-		ds->DrawLine(cx, cy, cx + ix, cy + iy, Colours::Snow, 1.0F);
+		circle_point(this->radius, radian, &ix, &iy);
+		ds->DrawLine(cx, cy, cx + ix, cy + iy, this->style.axes_color, 1.0F);
+	}
+
+	ds->FillGeometry(this->shape, cx, cy, this->style.shape_color);
+	ds->DrawGeometry(this->shape, cx, cy, this->style.outline_color, this->style.outline_thickness);
+}
+
+void IRadarlet::reshape() {
+	CanvasPathBuilder^ shape = ref new CanvasPathBuilder(CanvasDevice::GetSharedDevice());
+	double rad_step = 2.0 * pi / double(this->count);
+	float vx, vy;
+
+	circle_point(this->radius * this->scales[0], float(radian0), &vx, &vy);
+	shape->BeginFigure(vx, vy);
+
+	for (unsigned int idx = 1; idx < this->count; idx++) {
+		float radian = float(radian0 + rad_step * double(idx));
+
+		circle_point(this->radius * this->scales[idx], radian, &vx, &vy);
+		shape->AddLine(vx, vy);
+	}
+
+	shape->EndFigure(CanvasFigureLoop::Closed);
+
+	this->shape = CanvasGeometry::CreatePath(shape);
+}
+
+void IRadarlet::set_scale(unsigned int index, float v) {
+	if (index < this->count) {
+		this->scales[index] = flmax(flmin(v, 1.0F), 0.0F);
+		this->reshape();
+	}
+}
+
+void IRadarlet::set_scales(float* vs) {
+	for (unsigned int idx = 0; idx < this->count; idx++) {
+		this->scales[idx] = flmax(flmin(vs[idx], 1.0F), 0.0F);
+	}
+
+	this->reshape();
+}
+
+void IRadarlet::prepare_style() {
+	CAS_SLOT(this->style.font, make_bold_text_format(caption_size));
+
+	CAS_SLOT(this->style.outline_color, radar_default_color);
+	
+	{ // transparency filling
+		CanvasSolidColorBrush^ c = dynamic_cast<CanvasSolidColorBrush^>(this->style.outline_color);
+		if (c != nullptr) {
+			style.shape_color = Colours::make(c, 0.618);
+		}
+	}
+
+	CAS_SLOT(this->style.label_color, radar_default_label_color);
+	CAS_SLOT(this->style.axes_color, this->style.label_color);
+	CAS_SLOT(this->style.ring_color, radar_default_ring_color);
+
+	if (this->style.outline_thickness <= 0.0F) {
+		this->style.outline_thickness = 2.0F;
 	}
 }
--- graphlet/dashboard/radarlet.hpp
+++ graphlet/dashboard/radarlet.hpp
@@ -12,55 +12,79 @@
 namespace WarGrey::SCADA {
 	private struct RadarStyle {
 		Microsoft::Graphics::Canvas::Text::CanvasTextFormat^ font;
-		Microsoft::Graphics::Canvas::Text::CanvasTextFormat^ legend_font;
 
-		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ border_color;
-		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ haxes_color;
-		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ vaxes_color;
-		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ selected_color;
+		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ outline_color;
+		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ shape_color;
 
-		Microsoft::Graphics::Canvas::Geometry::CanvasStrokeStyle^ haxes_style;
-		Microsoft::Graphics::Canvas::Geometry::CanvasStrokeStyle^ vaxes_style;
-		Microsoft::Graphics::Canvas::Geometry::CanvasStrokeStyle^ lines_style;
-		Microsoft::Graphics::Canvas::Geometry::CanvasStrokeStyle^ selected_style;
+		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ label_color;
+		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ axes_color;
+		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ ring_color;
 
-		float lines_thickness = -1.0F;
-		float border_thickness = -1.0F;
-		float haxes_thickness = -1.0F;
-		float vaxes_thickness = -1.0F;
-		float selected_thickness = -1.0F;
-
-		float legend_fx = -1.0F;
-		float maximum_fx = -1.0F;
+		float outline_thickness = 1.5F;
 	};
 
 	/************************************************************************************************/
 	private class IRadarlet abstract : public WarGrey::SCADA::IGraphlet {
 	public:
-		IRadarlet(float radius, unsigned int count);
+		virtual ~IRadarlet() noexcept;
+		IRadarlet(float radius, unsigned int count, float* initials = nullptr);
+		IRadarlet(float radius, unsigned int count, WarGrey::SCADA::RadarStyle& style, float* initials = nullptr);
 
 	public:
 		void construct() override;
 		void fill_extent(float x, float y, float* w = nullptr, float* h = nullptr) override;
 		void draw(Microsoft::Graphics::Canvas::CanvasDrawingSession^ ds, float x, float y, float Width, float Height) override;
 
+	public:
+		void set_scales(float* vs);
+		void set_scale(unsigned int index, float v);
+
+	protected:
+		Platform::Array<Platform::String^>^ captions;
+
 	private:
-		Microsoft::Graphics::Canvas::Geometry::CanvasCachedGeometry^ vmarks;
-		Microsoft::Graphics::Canvas::Geometry::CanvasCachedGeometry^ vaxes;
-		Microsoft::Graphics::Canvas::Geometry::CanvasCachedGeometry^ hmarks;
-		Microsoft::Graphics::Canvas::Geometry::CanvasGeometry^ haxes;
+		void prepare_style();
+
+	private:
+		void reshape();
+
+	private:
+		Microsoft::Graphics::Canvas::Geometry::CanvasCachedGeometry^ caption;
+		Microsoft::Graphics::Canvas::Geometry::CanvasGeometry^ shape;
 
 	private:
 		WarGrey::SCADA::RadarStyle style;
 
 	private:
 		unsigned int count;
+		float* scales;
+		float Radius;
 		float radius;
 	};
 
 	template<typename E>
 	private class Radarlet : public WarGrey::SCADA::IRadarlet {
 	public:
-		Radarlet(float radius) : IRadarlet(radius, _N(E)) {}
+		Radarlet(Platform::String^ tongue, float radius, float* initials = nullptr)
+			: IRadarlet(radius, _N(E), initials) {
+			this->initialize_caption(tongue);
+		}
+
+		Radarlet(Platform::String^ tongue, float radius, WarGrey::SCADA::RadarStyle& style, float* initials = nullptr)
+			: IRadarlet(radius, _N(E), style, initials) {
+			this->initialize_caption(tongue);
+		}
+
+	public:
+		void set_scale(E id, float v) {
+			IRadarlet::set_scale(_I(id), v);
+		}
+
+	private:
+		void initialize_caption(Platform::String^ tongue) {
+			for (E id = _E0(E); id < E::_; id++) {
+				this->captions[_I(id)] = speak(id, tongue);
+			}
+		}
 	};
 }
