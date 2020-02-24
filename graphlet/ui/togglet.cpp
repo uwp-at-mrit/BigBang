4cb73ac242943be8749641703f8dbf38e6a366e4
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2020-02-24 10:42:04 +0800

    add ToggleStyle

--- graphlet/ui/togglet.cpp
+++ graphlet/ui/togglet.cpp
@@ -15,21 +15,38 @@ using namespace Microsoft::Graphics::Canvas;
 using namespace Microsoft::Graphics::Canvas::Text;
 using namespace Microsoft::Graphics::Canvas::Brushes;
 
+static Platform::String^ toggle_default_font_name = "Microsoft YaHei";
+static const float toggle_default_font_size = 16.0F;
+
 static CanvasSolidColorBrush^ toggle_default_checked_color = Colours::DodgerBlue;
 static CanvasSolidColorBrush^ toggle_default_unchecked_color = Colours::SlateGray;
 
-/**************************************************************************************************/
-Togglet::Togglet(bool initial_state, Platform::String^ checked_label, Platform::String^ unchecked_label, float width
-	, CanvasSolidColorBrush^ ckcolor, CanvasSolidColorBrush^ uncolor, Platform::String^ tongue)
-	: ckcolor(ckcolor == nullptr ? toggle_default_checked_color : ckcolor)
-	, uncolor(uncolor == nullptr ? toggle_default_unchecked_color : uncolor)
-	, state(initial_state), width(width) {
-	CanvasTextFormat^ font = make_text_format("Microsoft YaHei", 16.0F);
+ToggleStyle WarGrey::SCADA::make_toggle_style(float fontsize, Colour^ checked_color, Colour^ unchecked_color) {
+	ToggleStyle s;
 
-	this->label = make_text_layout(speak(checked_label, tongue), font);
-	this->unlabel = make_text_layout(speak(unchecked_label, tongue), font);
-	this->lblcolor = Colours::contrast(this->ckcolor);
-	this->unlblcolor = Colours::contrast(this->uncolor);
+	s.font = make_text_format(toggle_default_font_name, ((fontsize > 0.0F) ? fontsize : toggle_default_font_size));
+	s.checked_color = ((checked_color == nullptr) ? toggle_default_checked_color : checked_color);
+	s.unchecked_color = ((unchecked_color == nullptr) ? toggle_default_unchecked_color : unchecked_color);
+
+	return s;
+}
+
+/**************************************************************************************************/
+Togglet::Togglet(bool state0, Platform::String^ label, float width, Platform::String^ tongue)
+	: Togglet(make_toggle_style(), state0, label, width, tongue) {}
+
+Togglet::Togglet(ToggleStyle& style, bool state0, Platform::String^ label, float width, Platform::String^ tongue)
+	: Togglet(style, state0, label, label, width, tongue) {}
+
+Togglet::Togglet(bool initial_state, Platform::String^ checked_label, Platform::String^ unchecked_label, float width, Platform::String^ tongue)
+	: Togglet(make_toggle_style(), initial_state, checked_label, unchecked_label, width, tongue) {}
+
+Togglet::Togglet(ToggleStyle& style, bool initial_state, Platform::String^ checked_label, Platform::String^ unchecked_label, float width, Platform::String^ tongue)
+	: state(initial_state), width(width), style(style) {
+	this->prepare_style();
+
+	this->label = make_text_layout(speak(checked_label, tongue), this->style.font);
+	this->unlabel = make_text_layout(speak(unchecked_label, tongue), this->style.font);
 
 	this->state = initial_state;
 	this->width = width;
@@ -60,13 +77,13 @@ void Togglet::draw(CanvasDrawingSession^ ds, float x, float y, float Width, floa
 	bradius = radius * 0.80F;
 
 	if (this->state) {
-		ds->FillRoundedRectangle(x, y, width, diameter, radius, radius, this->ckcolor);
-		ds->DrawTextLayout(this->label, x + radius, y, this->lblcolor);
-		ds->FillCircle(float2(x + width - radius, y + radius), bradius, this->lblcolor);
+		ds->FillRoundedRectangle(x, y, width, diameter, radius, radius, this->style.checked_color);
+		ds->DrawTextLayout(this->label, x + radius, y, this->style.checked_label_color);
+		ds->FillCircle(float2(x + width - radius, y + radius), bradius, this->style.checked_label_color);
 	} else {
-		ds->FillRoundedRectangle(x, y, width, diameter, radius, radius, this->uncolor);
-		ds->DrawTextLayout(this->unlabel, x + width - radius - this->unlabel->LayoutBounds.Width, y, this->unlblcolor);
-		ds->FillCircle(float2(x + radius, y + radius), bradius, this->unlblcolor);
+		ds->FillRoundedRectangle(x, y, width, diameter, radius, radius, this->style.unchecked_color);
+		ds->DrawTextLayout(this->unlabel, x + width - radius - this->unlabel->LayoutBounds.Width, y, this->style.unchecked_label_color);
+		ds->FillCircle(float2(x + radius, y + radius), bradius, this->style.unchecked_label_color);
 	}
 }
 
@@ -77,4 +94,13 @@ bool Togglet::checked() {
 void Togglet::toggle() {
 	this->state = !this->state;
 	this->notify_updated();
+}
+
+void Togglet::prepare_style() {
+	CAS_SLOT(this->style.font, make_text_format(toggle_default_font_name, toggle_default_font_size));
+
+	CAS_SLOT(this->style.checked_color, toggle_default_checked_color);
+	CAS_SLOT(this->style.unchecked_color, toggle_default_unchecked_color);
+	CAS_SLOT(this->style.checked_label_color, Colours::contrast(this->style.checked_color));
+	CAS_SLOT(this->style.unchecked_label_color, Colours::contrast(this->style.unchecked_color));
 }
--- graphlet/ui/togglet.hpp
+++ graphlet/ui/togglet.hpp
@@ -4,11 +4,25 @@
 #include "brushes.hxx"
 
 namespace WarGrey::SCADA {
+	private struct ToggleStyle {
+		Microsoft::Graphics::Canvas::Text::CanvasTextFormat^ font;
+
+		WarGrey::SCADA::Colour^ checked_color;
+		WarGrey::SCADA::Colour^ unchecked_color;
+
+		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ checked_label_color;
+		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ unchecked_label_color;
+	};
+
+	WarGrey::SCADA::ToggleStyle make_toggle_style(float fontsize = 0.0F, WarGrey::SCADA::Colour^ checked_color = nullptr, WarGrey::SCADA::Colour^ unchecked_color = nullptr);
+
 	private class Togglet : public virtual WarGrey::SCADA::IGraphlet {
 	public:
-		Togglet(bool state0, Platform::String^ checked_label, Platform::String^ unchecked_label, float width = 0.0F,
-			WarGrey::SCADA::Colour^ checked_color = nullptr, WarGrey::SCADA::Colour^ unchecked_color = nullptr,
-			Platform::String^ tongue = "menu");
+		Togglet(bool state0, Platform::String^ label, float width = 0.0F, Platform::String^ tongue = "menu");
+		Togglet(WarGrey::SCADA::ToggleStyle& style, bool state0, Platform::String^ label, float width = 0.0F, Platform::String^ tongue = "menu");
+
+		Togglet(bool state0, Platform::String^ checked_label, Platform::String^ unchecked_label, float width = 0.0F, Platform::String^ tongue = "menu");
+		Togglet(WarGrey::SCADA::ToggleStyle& style, bool state0, Platform::String^ checked_label, Platform::String^ unchecked_label, float width = 0.0F, Platform::String^ tongue = "menu");
 
 	public:
 		void fill_extent(float x, float y, float* w = nullptr, float* h = nullptr) override;
@@ -19,12 +33,14 @@ namespace WarGrey::SCADA {
 		void toggle();
 
 	private:
+		void prepare_style();
+
+	private:
 		Microsoft::Graphics::Canvas::Text::CanvasTextLayout^ label;
 		Microsoft::Graphics::Canvas::Text::CanvasTextLayout^ unlabel;
-		Microsoft::Graphics::Canvas::Brushes::CanvasSolidColorBrush^ ckcolor;
-		Microsoft::Graphics::Canvas::Brushes::CanvasSolidColorBrush^ uncolor;
-		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ lblcolor;
-		Microsoft::Graphics::Canvas::Brushes::ICanvasBrush^ unlblcolor;
+
+	private:
+		WarGrey::SCADA::ToggleStyle style;
 
 	private:
 		float width;
